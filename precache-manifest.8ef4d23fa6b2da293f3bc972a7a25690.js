7f48c641a3617320f8289efad3e90897a0ea4fe1
gyeolse <threewave@kakao.com>
2020-12-11 17:49:58 +0900

    Updates

---
+++ precache-manifest.8ef4d23fa6b2da293f3bc972a7a25690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b75a8e31bf922d891abf815c153240",
+    "url": "/gyeolse.github.io/2048_games/index.html"
+  },
+  {
+    "revision": "59643257f52bc168a369",
+    "url": "/gyeolse.github.io/2048_games/static/css/main.a46e451d.chunk.css"
+  },
+  {
+    "revision": "ed5a3694a4887e8cf53a",
+    "url": "/gyeolse.github.io/2048_games/static/js/2.fac54e3d.chunk.js"
+  },
+  {
+    "revision": "c4f8edccbae19625bc18d751d192f057",
+    "url": "/gyeolse.github.io/2048_games/static/js/2.fac54e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59643257f52bc168a369",
+    "url": "/gyeolse.github.io/2048_games/static/js/main.b008866a.chunk.js"
+  },
+  {
+    "revision": "a20cf70aa731e90b2729",
+    "url": "/gyeolse.github.io/2048_games/static/js/runtime-main.027f5883.js"
+  }
+]);
